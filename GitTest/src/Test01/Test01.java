3036888dd9a45a9e104abb5f2fac81982f4cba94
Jeong HeeSeok <183115@jnu.ac.kr>
2024-12-23 11:28:26 +0900

    네 번재 테스트 - 작성

--- GitTest/src/Test01/Test01.java
+++ GitTest/src/Test01/Test01.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 	System.out.println("팀장 첫번째 커밋");
 	System.out.println("팀원1 두번째 커밋");
 	System.out.println("팀원2 오도영임미다? 데헷");
+	System.out.println("팀원3 네번재 커밋");
 }
 
 }
