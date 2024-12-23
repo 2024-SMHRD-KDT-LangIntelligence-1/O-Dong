cd8516bed3df614b4b71ca779215c50389445b95
SMHRD <SMHRD@192.168.219.55>
2024-12-23 11:32:29 +0900

    변경....해써....

--- GitTest/src/Test01/Test01.java
+++ GitTest/src/Test01/Test01.java
@@ -3,6 +3,9 @@ package Test01;
 public class Test01{
 public static void main(String[] args) {
 	System.out.println("팀장 첫번째 커밋");
+	System.out.println("팀원1 첫번째 커밋");
+	System.out.println("다섯번째 테스트..피부가 건조하다... 수분 챱챱");
+	
 }
 
 }
