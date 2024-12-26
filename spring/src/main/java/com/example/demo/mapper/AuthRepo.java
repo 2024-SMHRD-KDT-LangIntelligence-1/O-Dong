f4b58e8ca407f9be05c566b58f129f180d7cb0e0
김수지 <suji.kim.sweden@gmail.com>
2024-12-26 16:25:06 +0900

    Delete spring directory

--- spring/src/main/java/com/example/demo/mapper/AuthRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.mapper;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.AuthEntity;
-
-public interface AuthRepo extends JpaRepository<AuthEntity, String> {
-    AuthEntity findByUserIdAndUserPw(String user_id, String user_pw);
-}
